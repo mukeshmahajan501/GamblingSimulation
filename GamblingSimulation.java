1c5e8ab38121ac172b3ecc04efc07b2c3f43d25c
mukesh <mukeshmahajan501@gmail.com>
2020-07-30 14:27:33 +0530

    added code for win or loss

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,5 +1,22 @@
 public class GamblingSimulation{
 	//declare constant variable
-	final int STAKE_AMOUNT = 100;
-	final int BIT_AMOUNT = 1;
+	final static int STAKE_AMOUNT = 100;
+	final static int BIT_AMOUNT = 1;
+	
+	public static void main(String args[]){
+	
+	int randomCheck = (int)Math.floor(Math.random()*10%2);
+	if(randomCheck == 1)
+	{
+		System.out.println("you won $1");
+		int totalAmount=STAKE_AMOUNT+BIT_AMOUNT;
+	}
+	else
+	{
+		System.out.println("you loss $1");
+		int totalAmount=STAKE_AMOUNT-BIT_AMOUNT;
+	}
+
+}
+	
 }
