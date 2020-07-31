bb4843fdb2b0d686df488c44315186b6d2b5c680
mukesh <mukeshmahajan501@gmail.com>
2020-07-31 07:37:31 +0530

    resign 50 % stake if gambler won or lost

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,22 +1,45 @@
 public class GamblingSimulation{
-	//declare constant variable
-	final static int STAKE_AMOUNT = 100;
-	final static int BIT_AMOUNT = 1;
-	
-	public static void main(String args[]){
-	
-	int randomCheck = (int)Math.floor(Math.random()*10%2);
-	if(randomCheck == 1)
-	{
-		System.out.println("you won $1");
-		int totalAmount=STAKE_AMOUNT+BIT_AMOUNT;
-	}
-	else
-	{
-		System.out.println("you loss $1");
-		int totalAmount=STAKE_AMOUNT-BIT_AMOUNT;
-	}
+	//declare  variable
+	  int stake = 100;
+	  int bet = 1;
+
+	public void checkWinLoose()
+{
+        while(stake != 150 && stake != 50)
+        {
+                int bettingResult=(int)(Math.floor(Math.random()*10)%2);
+                if(bettingResult == 1)
+                {
+                        stake=stake+bet;
+                }
+                else
+                {
+                        stake=stake-bet;
+                }
+
+
+        }
+
+
+                if(stake == 150)
+                {
+                        System.out.println("you won the game");
+								System.out.println("stake: "+stake);
+                }
+                else
+                {
+                        System.out.println("you loose the game");
+								System.out.println("stake: "+stake);
+                }
+
 
 }
+
+	public static void main(String args[]){
+	
+	GamblingSimulation gs=new GamblingSimulation();
+	gs.checkWinLoose();
+}
 	
 }
+
