5dc820eceb68203c01f8bdf846ff403b3279ba4e
mukesh <mukeshmahajan501@gmail.com>
2020-07-31 09:02:10 +0530

    refactor the code for 20 days

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,45 +1,38 @@
-public class GamblingSimulation{
-	//declare  variable
-	  int stake = 100;
-	  int bet = 1;
 
-	public void checkWinLoose()
-{
-        while(stake != 150 && stake != 50)
-        {
-                int bettingResult=(int)(Math.floor(Math.random()*10)%2);
-                if(bettingResult == 1)
-                {
-                        stake=stake+bet;
-                }
-                else
-                {
-                        stake=stake-bet;
-                }
+public class GamblingSimulation {
 
+	public void checkWinLoose() {
 
-        }
+		for (int i = 1; i <= 20; i++) {
+			int stake = 100;
+			int bet = 1;
+			int totalAmount = 0;
+			while ((stake != 150) && (stake != 50)) {
+				int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
 
+				if (randomCheck == 1) {
+					stake = stake + bet;
 
-                if(stake == 150)
-                {
-                        System.out.println("you won the game");
-			System.out.println("stake: "+stake);
-                }
-                else
-                {
-                        System.out.println("you loose the game");
-		        System.out.println("stake: "+stake);
-                }
+				} else {
+					stake = stake - bet;
+				}
 
+			}
+
+			if (stake == 150) {
+				System.out.println("total Amount won in day:"+i);
+
+			} else {
+				System.out.println("total amount lost in days: "+i);
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+		GamblingSimulation obj = new GamblingSimulation();
+		obj.checkWinLoose();
+	}
 
 }
-
-	public static void main(String args[]){
-	
-	GamblingSimulation gs=new GamblingSimulation();
-	gs.checkWinLoose();
-}
-	
-}
-
