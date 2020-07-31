e4eccd4b11dfea338c3601830a4e19738ed82b98
mukesh <mukeshmahajan501@gmail.com>
2020-07-31 09:16:48 +0530

    refactor code for month and calculate total amount

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,38 +1,46 @@
-
 public class GamblingSimulation {
 
-	public void checkWinLoose() {
+        public void checkWinLoose() {
 
-		for (int i = 1; i <= 20; i++) {
-			int stake = 100;
-			int bet = 1;
-			int totalAmount = 0;
-			while ((stake != 150) && (stake != 50)) {
-				int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
+                for (int i = 1; i <= 30; i++) {
+                        int winCount = 0;
+                        int lostCount = 0;
+                        int stake = 100;
+                        int bet = 1;
+                        int totalAmount = 0;
+                        while ((stake != 150) && (stake != 50)) {
+                                int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
 
-				if (randomCheck == 1) {
-					stake = stake + bet;
+                                if (randomCheck == 1) {
+                                        stake = stake + bet;
+                                        winCount++;
 
-				} else {
-					stake = stake - bet;
-				}
+                                } else {
+                                        stake = stake - bet;
+                                        lostCount++;
+                                }
 
-			}
+                        }
 
-			if (stake == 150) {
-				System.out.println("total Amount won in day:"+i);
+                        if (stake == 150) {
+                                System.out.println("you won the game");
+                                totalAmount = winCount;
+                                System.out.println("total Amount won in day:"+i +" Amount:"+ totalAmount);
 
-			} else {
-				System.out.println("total amount lost in days: "+i);
-			}
+                        } else {
+                                System.out.println("you loose the game");
+                                totalAmount = lostCount;
+                                System.out.println("total amount lost in days: "+i +" Amount:"+ totalAmount);
+                        }
 
-		}
+                }
 
-	}
+        }
 
-	public static void main(String[] args) {
-		GamblingSimulation obj = new GamblingSimulation();
-		obj.checkWinLoose();
-	}
+        public static void main(String[] args) {
+                GamblingSimulation obj = new GamblingSimulation();
+                obj.checkWinLoose();
+        }
 
 }
+
